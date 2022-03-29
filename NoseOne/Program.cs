8f6222911c1ccf90af7892f2978dae1eb47ba9cc
Kasper Javelin <kasper.javelin@mail.com>
2022-03-29 14:15:01 +0200

    sdthdtyj

--- NoseOne/Program.cs
+++ NoseOne/Program.cs
@@ -6,9 +6,70 @@
 //     Console.WriteLine("Hellooooo");
 // }
 
+// string name = Console.ReadLine();
+// if (name == "noname"){
+//     Console.WriteLine("välkommen");
+
+// }
+
+// string name = Console.ReadLine();
+// string password = Console.ReadLine();
+
+// if (name == "noname"&& password == "nopass"){
+//     Console.WriteLine("välkommen");
+
+// }
+// else {
+//     Console.WriteLine("Wrong username or password");
+// }
+
+// for (int i = 0; i < 32; i++)
+// {
+//     Console.WriteLine("Hello");
+// }
 
 
+// string password = "";
+// while (password != "nopass")
+// {
+//     Console.WriteLine("Skriv ditt lösenord");
+//      password = Console.ReadLine();
+// }
+
+// for (int i = 0; i < 5; i++)
+// {
+//     string tal = Console.ReadLine();
+//     int talint = int.Parse(tal);
+//     if (talint > 5){
+//     Console.WriteLine("större en 5");
+//     }
+
+// }
 
 
+// Random rnd = new Random();
+// int tal = rnd.Next(0, 100);
+// string gissning = "";
+// bool klar = false;
 
-Console.ReadLine();
+// do
+// {
+//     int gissningInt;
+//     bool success = int.TryParse(Console.ReadLine(), out gissningInt);
+
+//     if (gissningInt == tal)
+//     {
+//         Console.WriteLine("Du hittade siffran");
+//         klar = true;
+//     }
+//     else if (gissningInt < tal)
+//     {
+//         Console.WriteLine("Du är under siffran");
+//     }
+//     else if (gissningInt > tal)
+//     {
+//         Console.WriteLine("Du är över siffran");
+//     }
+//     } while (!klar); 
+
+    Console.ReadLine();
